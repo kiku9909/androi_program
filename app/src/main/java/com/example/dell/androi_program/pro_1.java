b91f9259f5a5a20cf3d526d362bb3a98168fefc3
sourabh <karmakarsourabh7@gmail.com>
2018-07-10 22:13:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dell/androi_program/pro_1.java
@@ -0,0 +1,13 @@
+package com.example.dell.androi_program;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pro_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pro_1);
+    }
+}
